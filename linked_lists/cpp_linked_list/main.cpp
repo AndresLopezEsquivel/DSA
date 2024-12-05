9cd02c9c316409f35001b4f995adca35c4665d42
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2024-12-04 21:05:11 -0600

    Start working on C++ linked list

---
+++ linked_lists/cpp_linked_list/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+  public:
+  int value;
+  Node *next;
+
+  Node(int value) : value(value), next(nullptr) {}
+};
+
+int main()
+{
+  return 0;
+}
