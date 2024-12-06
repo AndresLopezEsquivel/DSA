9615ca3bbf968b97c94b77c827558b00442ca79e
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2024-12-05 21:24:18 -0600

    Add code for releasing  LinkedList's nodes memory, appending new nodes and printing nodes

--- linked_lists/cpp_linked_list/main.cpp
+++ linked_lists/cpp_linked_list/main.cpp
@@ -11,7 +11,76 @@ class Node
   Node(int value) : value(value), next(nullptr) {}
 };
 
+class LinkedList
+{
+  private:
+  Node *head;
+  Node *tail;
+  int length;
+
+  public:
+  LinkedList() : head(nullptr), tail(nullptr), length(0) {}
+  LinkedList(int value) : LinkedList() { appendNode(value); }
+
+  ~LinkedList()
+  {
+    Node *currentNode = head;
+    while(currentNode)
+    {
+      Node *tmp = currentNode;
+      currentNode = currentNode->next;
+      delete tmp;
+    }
+  }
+
+  void appendNode(int value)
+  {
+    Node *newNode = new Node(value);
+
+    if(tail)
+    {
+      tail->next = newNode;
+      tail = newNode;
+    }
+    else
+    {
+      head = newNode;
+      tail = newNode;
+    }
+    length++;
+  }
+
+  void printLinkedList()
+  {
+    if(!tail)
+    {
+      cout << "== Empty Linked List ==" << endl;
+      return;
+    }
+    else
+    {
+      Node *tmpNode = head;
+
+      while(tmpNode)
+      {
+        cout << tmpNode->value;
+        if(tmpNode->next)
+          cout << " -> ";
+        tmpNode = tmpNode->next;
+      }
+      cout << endl;
+    }
+  }
+};
+
 int main()
 {
+  LinkedList list(1);
+
+  list.appendNode(2);
+  list.appendNode(3);
+  list.appendNode(4);
+  list.printLinkedList();
+
   return 0;
 }
