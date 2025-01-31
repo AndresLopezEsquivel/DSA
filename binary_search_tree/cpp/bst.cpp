42bc8dabfa2d37e0172a20409b63670e274dada6
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2025-01-30 22:18:42 -0600

    Implement BST constructor

---
+++ binary_search_tree/cpp/bst.cpp
@@ -0,0 +1,3 @@
+#include "bst.h"
+
+BinarySearchTree::BinarySearchTree() : root{nullptr} {}
---
+++ binary_search_tree/cpp/bst.h
@@ -0,0 +1,12 @@
+#ifndef BST_H
+#define BST_H
+#include "node.h"
+class BinarySearchTree
+{
+  private:
+  Node* root;
+
+  public:
+  BinarySearchTree();
+};
+#endif
