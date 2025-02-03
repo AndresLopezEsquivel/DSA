a3564c9bb3d49b8363156492ea0459c9f8dc471e
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2025-02-03 15:45:16 -0600

    Overload ==, < and > for Node class

--- binary_search_tree/cpp/main.cpp
+++ binary_search_tree/cpp/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "node.h"
 
 int main()
 {
+  // Test operator= and operator< for Node
+  Node* node1 = new Node(1);
+  Node* node2 = new Node(2);
+  Node* node3 = new Node(1);
+
+  std::cout << std::boolalpha;
+  std::cout << "node1 == node2: " << (*node1 == node2) << std::endl; // false
+  std::cout << "node1 == node3: " << (*node1 == node3) << std::endl; // true
+  std::cout << "node1 < node2: " << (*node1 < node2) << std::endl; // true
+  std::cout << "node1 < node3: " << (*node1 < node3) << std::endl; // false
+  std::cout << "node1 > node2: " << (*node1 > node2) << std::endl; // false
+  std::cout << "node1 > node3: " << (*node1 > node3) << std::endl; // false
+  std::cout << "node3 > node1: " << (*node3 > node1) << std::endl; // false
   return 0;
 }
--- binary_search_tree/cpp/node.cpp
+++ binary_search_tree/cpp/node.cpp
@@ -1,3 +1,18 @@
 #include "node.h"
 
 Node::Node(int value) : value{value}, left{nullptr}, right{nullptr} {}
+
+bool Node::operator==(Node* node)
+{
+  return this->value == node->value;
+}
+
+bool Node::operator<(Node* node)
+{
+  return this->value < node->value;
+}
+
+bool Node::operator>(Node* node)
+{
+  return this->value > node->value;
+}
